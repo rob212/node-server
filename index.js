7c3b09e63ba91146eef0a66f8da2d7bfd0109d11
Robert McBryde <robert.mcbryde@ros.gov.uk>
2020-03-26 16:59:04 +0000

    playing with node as a server

---
+++ index.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
+const laptopData = JSON.parse(json);
+
+console.log(laptopData);
+
+
+const server = http.createServer((req, res) => {
+
+    const pathName = url.parse(req.url, true).pathname;
+    const id = url.parse(req.url, true).query.id;
+
+    if (pathName === '/products' || pathName === '/') {
+        res.writeHead(200, { 'Content-type': 'text/html'});
+        res.end('This is the products page');
+    } else if (pathName === '/laptop' && id < laptopData.length) {
+        res.writeHead(200, { 'Content-type': 'text/html'});
+        res.end(`This is the laptop page for laptop: ${id}`);
+    } else {
+        res.writeHead(404, { 'Content-type': 'text/html'});
+        res.end('This page is not found');
+    }
+
+});
+
+server.listen(1337, '127.0.0.1', () => {
+    console.log('Listening for requests in server');
+});
+
